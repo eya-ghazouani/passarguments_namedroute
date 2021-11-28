a3e1a12c3fcd8c558510414830daf85232703dea
eya-ghazouani <81834173+eya-ghazouani@users.noreply.github.com>
2021-11-29 00:58:26 +0100

    Register the widget in the routes table

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        routes: {
+          ExtractArgumentsScreen.routeName: (context) =>
+          const ExtractArgumentsScreen(),
+        },
     );
   }
 }
