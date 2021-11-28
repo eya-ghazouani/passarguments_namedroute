568125ef0638a52d35b0660cec6c40d7db379120
eya-ghazouani <81834173+eya-ghazouani@users.noreply.github.com>
2021-11-29 00:48:01 +0100

    Create a widget that extracts the arguments

--- lib/main.dart
+++ lib/main.dart
@@ -74,6 +74,30 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+class ExtractArgumentsScreen extends StatelessWidget {
+  const ExtractArgumentsScreen({Key? key}) : super(key: key);
+
+  static const routeName = '/extractArguments';
+
+  @override
+  Widget build(BuildContext context) {
+    // Extract the arguments from the current ModalRoute
+    // settings and cast them as ScreenArguments.
+    final args = ModalRoute.of(context)!.settings.arguments as ScreenArguments;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(args.title),
+      ),
+      body: Center(
+        child: Text(args.message),
+      ),
+    );
+  }
+}
+
+
 class ScreenArguments {
   final String title;
   final String message;
